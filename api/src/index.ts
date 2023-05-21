864cf3d9ab1a64c67c40fe6f5edb3a3620d6efbd
Meynard Duran <pvt_mduran18@yahoo.com>
2023-05-21 13:37:52 -0700

    created env, config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { config } from './config/config.js';
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+mongoose
+   .connect(config.mongo.url)
+   .then(() => console.log(`connected today! ${config.server.port}`))
+   .catch((error) => console.log(error));
+// const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-   res.send('hello! bro whats up?!!!');
+   res.send('get route ,test!!!!!!!!');
 });
 
-app.listen(port, () => {
-   console.log('Server is up and connected today ' + port);
+app.listen(`${config.server.port}`, () => {
+   console.log('Server is up and connected ' + `${config.server.port}`);
 });
